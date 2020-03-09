a02250edeb508a1a63c3103314dab8a5a39224d7
Megan Christine McClure <mcclme02@luther.edu>
2020-03-09 16:51:55 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,9 +4,9 @@ def my_all?(collection)
   i = 0 
   block_return_values = []
   while i < collection.length 
-  binding.pry 
     block_return_values << yield(collection[i])
     i += 1 
+    binding.pry 
   end 
 
 # my_all?([1,2,3]) {|i| i < 2}
